6c3247968f68aae609d5de44e0f8ea39b37ffd23
kaing615 <kainguyen615@gmail.com>
2025-06-25 01:42:30 +0700

    Add main API router and modularize route structure

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,9 @@ import mongoose from "mongoose";
 import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpecs from "./swagger.js";
+import cookieParser from "cookie-parser";
+import http from "http";
+import routes from "./routes/index.js";
 
 dotenv.config();
 
@@ -12,6 +15,10 @@ const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/v1", routes);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
@@ -19,12 +26,15 @@ app.get("/", (req, res) => {
   res.send("Welcome to the MentorMe backend!");
 });
 
+const server = http.createServer(app);
+
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
+      console.log(`Swagger docs at http://localhost:${PORT}/api-docs`);
     });
   })
   .catch((err) => {
