0b52bc3e545cc4f224690f76eb7e22d300b415b5
kaing615 <kainguyen615@gmail.com>
2025-06-25 01:17:48 +0700

    Add Swagger UI

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerSpecs from "./swagger.js";
 
 dotenv.config();
 
@@ -11,18 +13,21 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the MentorMe backend!');
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the MentorMe backend!");
 });
 
-mongoose.connect(process.env.MONGO_URL)
-    .then(() => {
-        console.log("MongoDB connected");
-        app.listen(PORT, () => {
-            console.log(`Server is running on http://localhost:${PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.log("MongoDB connection error: ", err);
-        process.exit(1);
-    })
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error: ", err);
+    process.exit(1);
+  });
