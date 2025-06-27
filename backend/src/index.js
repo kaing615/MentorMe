36cad66d4d001f8f24aa0be9ce86c838e4039c4c
kaing615 <kainguyen615@gmail.com>
2025-06-27 17:53:31 +0700

    Add security helmet, log morgan

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,14 +2,38 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from "mongoose";
 import cors from "cors";
+import path from 'path';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(cors());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan('common'));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.json());
+app.use("/assets",express.static(path.join(__dirname, 'public/assets')));
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/assets');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
 
 app.get('/', (req, res) => {
     res.send('Welcome to the MentorMe backend!');
