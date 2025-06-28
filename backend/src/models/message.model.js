504a4068a5fb481702e32e6870ef64cead93f4ff
kaing615 <kainguyen615@gmail.com>
2025-06-28 17:56:58 +0700

    Add message, profile, review, user model

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const MessageSchema = new mongoose.Schema({
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    messageType: { type: String, enum: ['text', 'image', 'file'], default: 'text' },
+    attachments: [{
+        url: String,
+        name: String, 
+        type: String,
+    }],
+    status: { type: String, enum: ['sent', 'delivered'], default: 'sent' },
+    content: { type: String, required: true },
+    sentAt: { type: Date, default: Date.now },
+    read: { type: Boolean, default: false },
+}, { timestamps: true });
