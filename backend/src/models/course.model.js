6fb879d6acf2522fbb13b08a5e09bc9ab916c728
kaing615 <kainguyen615@gmail.com>
2025-06-28 19:09:38 +0700

    Add full models MVP

---
+++ backend/src/models/course.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const CourseSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    mentor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    mentees: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    category: { type: String, required: true },
+    tags: [{ type: String }],
+    duration: { type: Number, required: true },
+    rate: { type: Number, default: 0 },
+    link: { type: String, required: true },
+    lectures: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Course", CourseSchema);
--- backend/src/models/profile.model.js
+++ backend/src/models/profile.model.js
@@ -1,21 +1,29 @@
 import mongoose from "mongoose";
 
-const ProfileSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    headline: { type: String, default: '' },
-    bio: { type: String, default: '' },
-    skills: { type: [String], default: [] },
-    experience: { type: String, default: '' },
-    description: { type: String, default: '' },
-    goal: { type: String, default: '' },
+const ProfileSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    headline: { type: String, default: "" },
+    bio: { type: String, default: "" }, //mentor
+    skills: { type: [String], default: [] }, //mentor
+    experience: { type: String, default: "" }, //mentor
+    description: { type: String, default: "" }, //mentee
+    goal: { type: String, default: "" }, //mentee
+    education: { type: String, default: "" }, //mentee
+    languages: { type: [String], default: [] }, //mentor, mentee
+    timezone: { type: String, default: "" }, //mentor, mentee
+    reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }], //mentor
+    rate: { type: Number, default: 0 }, //mentor
     links: {
-        website: { type: String, default: '' },
-        X: { type: String, default: '' },
-        linkedin: { type: String, default: '' },
-        github: { type: String, default: '' },
-        youtube: { type: String, default: '' },
-        facebook: { type: String, default: '' },
+      website: { type: String, default: "" },
+      X: { type: String, default: "" },
+      linkedin: { type: String, default: "" },
+      github: { type: String, default: "" },
+      youtube: { type: String, default: "" },
+      facebook: { type: String, default: "" },
     },
-}); 
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Profile", ProfileSchema);
