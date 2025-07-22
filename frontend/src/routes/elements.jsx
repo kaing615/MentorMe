27657dfe97996857bf5db5900cb2b635390666c0
TMQ2k <tmquang23@clc.fitus.edu.vn>
2025-07-22 22:55:27 +0700

    Update elements.jsx

--- frontend/src/routes/elements.jsx
+++ frontend/src/routes/elements.jsx
@@ -1,6 +1,6 @@
 import { PATH, AUTH_PATH, ADMIN_PATH, MENTEE_PATH, MENTOR_PATH } from "./path";
 import { useRoutes, Navigate } from "react-router-dom";
-import Home from "../pages/Home";
+// import Home from "../pages/Home";
 import Header from "../components/Layout/Header";
 
 const useRouterElements = () => {
