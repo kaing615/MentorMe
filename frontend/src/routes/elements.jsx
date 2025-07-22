df1423bd0910bd0c2b5f974452608ce84ec801dc
TMQ2k <tmquang23@clc.fitus.edu.vn>
2025-07-22 22:48:37 +0700

    Update layout header
    Tôi có làm thử sẵn layout header cho website mọi người xài thì có gì mọi người lấy về rồi làm phần task của mình nha.

--- frontend/src/routes/elements.jsx
+++ frontend/src/routes/elements.jsx
@@ -1,11 +1,13 @@
 import { PATH, AUTH_PATH, ADMIN_PATH, MENTEE_PATH, MENTOR_PATH } from "./path";
 import { useRoutes, Navigate } from "react-router-dom";
+import Home from "../pages/Home";
+import Header from "../components/Layout/Header";
 
 const useRouterElements = () => {
     const elements = useRoutes([
         {
             path: "/",
-            element: <Navigate to={`${PATH.MENTEE}`} replace />
+            element: <Header />
         },
     ]);
 
